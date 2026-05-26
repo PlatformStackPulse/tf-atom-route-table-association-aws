5aff4148e7907f2cc4d1beb3069f87daa39ddf0e
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 23:14:09 +0100

    feat: implement tf-atom-route-table-association-aws module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "enabled" {
-  description = "Whether the module is enabled."
+  description = "Whether the module is enabled"
   value       = local.enabled
+}
+
+output "id" {
+  description = "ID of the route table association"
+  value       = try(aws_route_table_association.this[0].id, null)
 }

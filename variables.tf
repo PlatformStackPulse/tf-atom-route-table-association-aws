9c946e96a3ffd2e28ef1913722a4a900c7e6d78e
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 23:14:09 +0100

    feat: implement tf-atom-route-table-association-aws module

--- variables.tf
+++ variables.tf
@@ -1,7 +1,17 @@
-# -----------------------------------------------------------------------------
-# Module-Specific Variables
-#
-# Note: Standard labeling variables (enabled, namespace, tenant, environment,
-# stage, name, delimiter, attributes, tags, label_order, etc.) are provided
-# by context.tf via the tf-label module.
-# -----------------------------------------------------------------------------
+variable "subnet_id" {
+  description = "ID of the subnet"
+  type        = string
+  validation {
+    condition     = length(var.subnet_id) > 0
+    error_message = "subnet_id must not be empty."
+  }
+}
+
+variable "route_table_id" {
+  description = "ID of the route table"
+  type        = string
+  validation {
+    condition     = length(var.route_table_id) > 0
+    error_message = "route_table_id must not be empty."
+  }
+}
